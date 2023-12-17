1facf1c15505467fc86df34721db836be2972ca1
Mirek Simek <miroslav.simek@gmail.com>
2023-12-17 17:48:11 +0100

    Fixing tests

--- tests/test_ui/test_ui_resource.py
+++ tests/test_ui/test_ui_resource.py
@@ -1,4 +1,9 @@
 import json
+from oarepo import __version__ as oarepo_version
+is_oarepo_11 = oarepo_version.split(".")[0] == "11"
+
+# RDM 12 adds "delete" to allowed actions (to be able to delete the request)
+allowed_actions = ["submit"] if is_oarepo_11 else ["submit", "delete"]
 
 
 def test_draft_publish_request_present(
@@ -14,7 +19,7 @@ def test_draft_publish_request_present(
     with client_with_login.get(f"/thesis/{example_topic_draft['id']}/edit") as c:
         assert c.status_code == 200
         data = json.loads(c.text)
-        assert data["available_requests"]["publish_draft"] == {'actions': ['submit', 'delete'], 'receiver': None, 'status': 'created', 'type': 'publish_draft'}
+        assert data["available_requests"]["publish_draft"] == {'actions': allowed_actions, 'receiver': None, 'status': 'created', 'type': 'publish_draft'}
         assert data["form_config"]["publish_draft"] == {'actions': ['submit', 'delete'], 'receiver': None, 'status': 'created', 'type': 'publish_draft'}
 
 
@@ -36,7 +41,7 @@ def test_record_delete_request_present(
         assert data["delete_record"]['type'] == "delete_record"
         assert data["delete_record"]["receiver"] is None
         assert data["delete_record"]["status"] == "created"
-        assert data["delete_record"]["actions"] == ["submit", "delete"]
+        assert data["delete_record"]["actions"] == allowed_actions
 
 
 def test_record_delete_unauthorized(
