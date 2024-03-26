33ad76d35e095201d3c862be611bd17004c17e4a
Martin Čorovčák <martino.coro@gmail.com>
2024-03-26 17:48:41 +0100

    Add initial load of requests via API request

--- oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/record-requests/components/RecordRequests.jsx
+++ oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/record-requests/components/RecordRequests.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
+import axios from "axios";
 import _sortBy from "lodash/sortBy";
+import _isEmpty from "lodash/isEmpty";
 
 import { CreateRequestButtonGroup, RequestListContainer } from ".";
 import { RequestContextProvider, RecordContextProvider } from "../contexts";
@@ -15,7 +17,24 @@ export const RecordRequests = ({ record }) => {
   /** @type {RequestType[]} */
   const requestTypes = record.request_types ?? [];
 
-  const requestsState = useState(_sortBy(record.requests, ["status_code"]) ?? []);
+  const requestsState = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: record.links?.requests,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.inveniordm.v1+json'
+      }
+    })
+      .then(response => {
+        requestsState[1](_sortBy(response.data?.hits?.hits, ["status_code"]));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <RecordContextProvider record={record}>
@@ -23,7 +42,7 @@ export const RecordRequests = ({ record }) => {
         {requestTypes && (
           <CreateRequestButtonGroup requestTypes={requestTypes} />
         )}
-        {record?.requests && (
+        {!_isEmpty(requestsState[0]) && (
           <RequestListContainer requestTypes={requestTypes} />
         )}
       </RequestContextProvider>
