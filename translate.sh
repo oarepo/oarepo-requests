9a26a017755c62d1146e65c3b52068af2afd069c
Mirek Simek <miroslav.simek@gmail.com>
2024-03-27 18:34:39 +0100

    removed obsolete translate command

--- translate.sh
+++
@@ -1,3 +0,0 @@
-pybabel extract -o oarepo_requests/translations/messages.pot oarepo_requests
-pybabel update -i oarepo_requests/translations/messages.pot -d oarepo_requests/translations
-pybabel compile -d oarepo_requests/translations
