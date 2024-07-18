36a3910e214b6920d5c29d71acbcdae53848b984
Miroslav Bauer <mirekys@gmail.com>
2024-07-18 14:56:49 +0200

    fix(deps): add missing bluebird dependency

--- oarepo_requests/ui/theme/webpack.py
+++ oarepo_requests/ui/theme/webpack.py
@@ -11,7 +11,9 @@ theme = WebpackThemeBundle(
                 "oarepo_requests_ui_request_detail": "./js/oarepo_requests_ui/request-detail/index.js",
                 "oarepo_requests_ui_components": "./js/oarepo_requests_ui/custom-components.js",
             },
-            dependencies={},
+            dependencies={
+                "bluebird": "^3.7.2",
+            },
             devDependencies={},
             aliases={
                 "@translations/oarepo_requests_ui": "translations/oarepo_requests_ui",
