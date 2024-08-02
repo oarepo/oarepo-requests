f891e62dfc9d28edac5192ffd6cb3f0a24cd48ab
Ronald Krist <ronald.krist@cesnet.cz>
2024-08-02 14:15:06 +0200

    using get_record+service_for_record from oarepo_runtime

--- oarepo_requests/services/schema.py
+++ oarepo_requests/services/schema.py
@@ -4,9 +4,8 @@ from invenio_records_resources.services.base.links import Link, LinksTemplate
 from invenio_requests.proxies import current_request_type_registry
 from invenio_requests.services.schemas import GenericRequestSchema
 from marshmallow import fields
+from oarepo_runtime.datastreams.utils import get_record_service_for_record
 from oarepo_runtime.records import is_published_record
-
-from oarepo_requests.utils import get_matching_service_for_record
 
 
 def get_links_schema():
@@ -31,7 +30,7 @@ class RequestTypeSchema(ma.Schema):
         type_id = data["type_id"]
         type = current_request_type_registry.lookup(type_id, quiet=True)
         record = self.context["record"]
-        service = get_matching_service_for_record(record)
+        service = get_record_service_for_record(record)
         link = ConditionalLink(
             cond=is_published_record,
             if_=Link(f"{{+api}}{service.config.url_prefix}{{id}}/requests/{type_id}"),
