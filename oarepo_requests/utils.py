18286f14c6c638efe2043b1ab5bde9bb3f207e68
Ronald Krist <ronald.krist@cesnet.cz>
2024-10-16 12:59:30 +0200

    deleted comment

--- oarepo_requests/utils.py
+++ oarepo_requests/utils.py
@@ -162,8 +162,6 @@ def get_receiver_for_request_type(request_type, identity, topic):
         return None
 
 
-    # creator = ResolverRegistry.reference_identity(identity)
-    # todo - the problem here is that we don't know who the creator is - it can be any of the users community roles at least?
     receivers = workflow_request.reference_receivers(
         identity=identity, record=topic, request_type=request_type, creator=identity
     )
