292231fd122e41033367a400e3785b6ad356bd50
ducica <dusanstojanovic256@gmail.com>
2024-12-01 14:39:09 +0100

    checking length of comment

--- oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/common/components/RequestCommentInput.jsx
+++ oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/common/components/RequestCommentInput.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { RichEditor } from "react-invenio-forms";
 import sanitizeHtml from "sanitize-html";
 import PropTypes from "prop-types";
 import { useQuery } from "@tanstack/react-query";
 import { httpApplicationJson } from "@js/oarepo_ui";
+import { i18next } from "@translations/oarepo_requests_ui/i18next";
+import { Message, Icon } from "semantic-ui-react";
 
 export const RequestCommentInput = ({
   comment,
   handleChange,
   initialValue,
+  maxCommentLength,
 }) => {
   // when focused move the cursor at the end of any existing content
   const handleFocus = (event, editor) => {
@@ -16,6 +19,8 @@ export const RequestCommentInput = ({
     editor.selection.collapse(false);
   };
 
+  const [pasteError, setPasteError] = useState(false);
+  const [length, setLength] = useState(initialValue?.length);
   // TODO: there is no appropriate URL to call here. I think this one is the safest, because we know it exists and it does
   // not rely on external library (like those that contain /me that are from dashboard). To be discussed how to handle this appropriately.
   // maybe some link that lives in oarepo ui and that can universaly provide allowed tags and attributes
@@ -30,26 +35,82 @@ export const RequestCommentInput = ({
 
   const allowedHtmlAttrs = data?.data?.allowedHtmlAttrs;
   const allowedHtmlTags = data?.data?.allowedHtmlTags;
-  return (
-    <RichEditor
-      initialValue={initialValue}
-      inputValue={comment}
-      editorConfig={{
-        auto_focus: true,
-        min_height: 100,
-        toolbar:
-          "blocks | bold italic | bullist numlist | outdent indent | undo redo",
-      }}
-      onEditorChange={(event, editor) => {
-        const cleanedContent = sanitizeHtml(editor.getContent(), {
-          allowedTags: allowedHtmlTags,
-          allowedAttributes: allowedHtmlAttrs,
-        });
 
-        handleChange(event, cleanedContent);
-      }}
-      onFocus={handleFocus}
-    />
+  return (
+    <React.Fragment>
+      <RichEditor
+        initialValue={initialValue}
+        inputValue={comment}
+        editorConfig={{
+          auto_focus: true,
+          min_height: 100,
+          toolbar:
+            "blocks | bold italic | bullist numlist | outdent indent | undo redo",
+          setup: (editor) => {
+            editor.on("BeforeAddUndo", (event) => {
+              const length = editor.getContent({ format: "text" }).length;
+              if (length >= maxCommentLength) {
+                event.preventDefault();
+              }
+            });
+            editor.on("PastePreProcess", (event) => {
+              const pastedText = event.content;
+              const sanitizedText = sanitizeHtml(pastedText, {
+                allowedTags: allowedHtmlTags,
+                allowedAttributes: allowedHtmlAttrs,
+              });
+              const currentCommentLength = editor.getContent({
+                format: "text",
+              }).length;
+              if (
+                sanitizedText.length + currentCommentLength >
+                maxCommentLength
+              ) {
+                event.content = sanitizedText.slice(
+                  0,
+                  maxCommentLength - currentCommentLength
+                );
+                setPasteError(true);
+                setTimeout(() => setPasteError(false), 3000);
+              }
+            });
+            editor.on("init", () => {
+              setLength(editor.getContent({ format: "text" }).length);
+            });
+          },
+        }}
+        onEditorChange={(event, editor) => {
+          const cleanedContent = sanitizeHtml(editor.getContent(), {
+            allowedTags: allowedHtmlTags,
+            allowedAttributes: allowedHtmlAttrs,
+          });
+          const textContent = editor.getContent({ format: "text" });
+          const textLength = textContent.length;
+
+          if (textLength <= maxCommentLength) {
+            handleChange(event, cleanedContent);
+          }
+          if (textContent.length !== length) {
+            if (textContent.trim().length === 0) {
+              setLength(0);
+            } else {
+              setLength(textLength);
+            }
+          }
+        }}
+        onFocus={handleFocus}
+      />
+      <small>{`${i18next.t("Remaining characters: ")}${
+        maxCommentLength - length
+      }`}</small>
+      {pasteError && (
+        <Message size="mini">
+          <Message.Content>
+            {i18next.t("Pasted text was shortened to fit the allowed length.")}
+          </Message.Content>
+        </Message>
+      )}
+    </React.Fragment>
   );
 };
 
@@ -57,8 +118,10 @@ RequestCommentInput.propTypes = {
   comment: PropTypes.string,
   handleChange: PropTypes.func,
   initialValue: PropTypes.string,
+  maxCommentLength: PropTypes.number,
 };
 
 RequestCommentInput.defaultProps = {
   initialValue: "",
+  maxCommentLength: 1000,
 };
