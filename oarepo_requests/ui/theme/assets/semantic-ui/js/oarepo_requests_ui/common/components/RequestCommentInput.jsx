31aaf9e639da7260d31722fe6ecdec4850222304
ducica <dusanstojanovic256@gmail.com>
2024-12-31 14:24:38 +0100

    made comment input overridable

--- oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/common/components/RequestCommentInput.jsx
+++ oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/common/components/RequestCommentInput.jsx
@@ -6,8 +6,9 @@ import { useQuery } from "@tanstack/react-query";
 import { httpApplicationJson } from "@js/oarepo_ui";
 import { i18next } from "@translations/oarepo_requests_ui/i18next";
 import { Message, Icon } from "semantic-ui-react";
+import Overridable from "react-overridable";
 
-export const RequestCommentInput = ({
+const CommentInput = ({
   comment,
   handleChange,
   initialValue,
@@ -37,93 +38,108 @@ export const RequestCommentInput = ({
   const allowedHtmlTags = data?.data?.allowedHtmlTags;
 
   return (
-    <React.Fragment>
-      <RichEditor
-        initialValue={initialValue}
-        inputValue={comment}
-        editorConfig={{
-          auto_focus: true,
-          min_height: 100,
-          width: "100%",
-          toolbar:
-            "blocks | bold italic | bullist numlist | outdent indent | undo redo",
-          setup: (editor) => {
-            editor.on("BeforeAddUndo", (event) => {
-              const length = editor.getContent({ format: "text" }).length;
-              if (length >= maxCommentLength) {
-                event.preventDefault();
-              }
-            });
-            editor.on("PastePreProcess", (event) => {
-              const pastedText = event.content;
-              const sanitizedText = sanitizeHtml(pastedText, {
-                allowedTags: allowedHtmlTags,
-                allowedAttributes: allowedHtmlAttrs,
+    <Overridable
+      id="OarepoRequests.RequestCommentInput"
+      comment={comment}
+      handleChange={handleChange}
+      initialValue={initialValue}
+      maxCommentLength={maxCommentLength}
+    >
+      <React.Fragment>
+        <RichEditor
+          initialValue={initialValue}
+          inputValue={comment}
+          editorConfig={{
+            auto_focus: true,
+            min_height: 100,
+            width: "100%",
+            toolbar:
+              "blocks | bold italic | bullist numlist | outdent indent | undo redo",
+            setup: (editor) => {
+              editor.on("BeforeAddUndo", (event) => {
+                const length = editor.getContent({ format: "text" }).length;
+                if (length >= maxCommentLength) {
+                  event.preventDefault();
+                }
               });
-              const currentCommentLength = editor.getContent({
-                format: "text",
-              }).length;
-              if (
-                sanitizedText.length + currentCommentLength >
-                maxCommentLength
-              ) {
-                event.content = sanitizedText.slice(
-                  0,
-                  maxCommentLength - currentCommentLength
-                );
-                setPasteError(true);
-                clearTimeout(timeoutId);
-                timeoutId = setTimeout(() => setPasteError(false), 3000);
-              }
+              editor.on("PastePreProcess", (event) => {
+                const pastedText = event.content;
+                const sanitizedText = sanitizeHtml(pastedText, {
+                  allowedTags: allowedHtmlTags,
+                  allowedAttributes: allowedHtmlAttrs,
+                });
+                const currentCommentLength = editor.getContent({
+                  format: "text",
+                }).length;
+                if (
+                  sanitizedText.length + currentCommentLength >
+                  maxCommentLength
+                ) {
+                  event.content = sanitizedText.slice(
+                    0,
+                    maxCommentLength - currentCommentLength
+                  );
+                  setPasteError(true);
+                  clearTimeout(timeoutId);
+                  timeoutId = setTimeout(() => setPasteError(false), 3000);
+                }
+              });
+              editor.on("init", () => {
+                setLength(editor.getContent({ format: "text" }).length);
+              });
+            },
+          }}
+          onEditorChange={(event, editor) => {
+            const cleanedContent = sanitizeHtml(editor.getContent(), {
+              allowedTags: allowedHtmlTags,
+              allowedAttributes: allowedHtmlAttrs,
             });
-            editor.on("init", () => {
-              setLength(editor.getContent({ format: "text" }).length);
-            });
-          },
-        }}
-        onEditorChange={(event, editor) => {
-          const cleanedContent = sanitizeHtml(editor.getContent(), {
-            allowedTags: allowedHtmlTags,
-            allowedAttributes: allowedHtmlAttrs,
-          });
-          const textContent = editor.getContent({ format: "text" });
-          const textLength = textContent.length;
+            const textContent = editor.getContent({ format: "text" });
+            const textLength = textContent.length;
 
-          if (textLength <= maxCommentLength) {
-            handleChange(event, cleanedContent);
-          }
-          // querky  behavior of the editor, when the content is empty, the length is 1
-          if (textContent.trim().length === 0) {
-            setLength(0);
-          } else {
-            setLength(textLength);
-          }
-        }}
-        onFocus={handleFocus}
-      />
-      <small>{`${i18next.t("Remaining characters: ")}${
-        maxCommentLength - length
-      }`}</small>
-      {pasteError && (
-        <Message size="mini">
-          <Message.Content>
-            <Icon name="warning circle" />
-            {i18next.t("Pasted text was shortened to fit the allowed length.")}
-          </Message.Content>
-        </Message>
-      )}
-    </React.Fragment>
+            if (textLength <= maxCommentLength) {
+              handleChange(event, cleanedContent);
+            }
+            // querky  behavior of the editor, when the content is empty, the length is 1
+            if (textContent.trim().length === 0) {
+              setLength(0);
+            } else {
+              setLength(textLength);
+            }
+          }}
+          onFocus={handleFocus}
+        />
+        <small>{`${i18next.t("Remaining characters: ")}${
+          maxCommentLength - length
+        }`}</small>
+        {pasteError && (
+          <Message size="mini">
+            <Message.Content>
+              <Icon name="warning circle" />
+              {i18next.t(
+                "Pasted text was shortened to fit the allowed length."
+              )}
+            </Message.Content>
+          </Message>
+        )}
+      </React.Fragment>
+    </Overridable>
   );
 };
 
-RequestCommentInput.propTypes = {
+export const RequestCommentInput = Overridable.component(
+  "RequestCommentInput",
+  CommentInput
+);
+
+CommentInput.propTypes = {
   comment: PropTypes.string,
   handleChange: PropTypes.func,
   initialValue: PropTypes.string,
   maxCommentLength: PropTypes.number,
 };
 
-RequestCommentInput.defaultProps = {
+CommentInput.defaultProps = {
   initialValue: "",
   maxCommentLength: 1000,
 };
