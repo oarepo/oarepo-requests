b2e9d11f7d44ac714113109586e90c4fc9660d0d
Mirek Simek <miroslav.simek@gmail.com>
2025-03-17 08:49:32 +0100

    Embedded filter arguments

--- oarepo_requests/models.py
+++ oarepo_requests/models.py
@@ -1,11 +1,12 @@
-from datetime import datetime
 import logging
-from sqlalchemy.exc import IntegrityError, SQLAlchemyError, NoResultFound
+from datetime import datetime
+
 import sqlalchemy as sa
 from invenio_db import db
-from sqlalchemy.dialects import mysql, postgresql
-from sqlalchemy_utils.types import JSONType, UUIDType
 from invenio_requests.records.models import RequestMetadata
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.exc import IntegrityError, NoResultFound, SQLAlchemyError
+from sqlalchemy_utils.types import JSONType, UUIDType
 
 logger = logging.getLogger("record-snapshot-table")
 
@@ -15,7 +16,8 @@ class RecordSnapshot(db.Model):
 
     It is possible that single record can have multiple snapshots.
     """
-    __tablename__ = 'record_snapshots'
+
+    __tablename__ = "record_snapshots"
 
     __table_args__ = (
         # TODO
@@ -27,7 +29,8 @@ class RecordSnapshot(db.Model):
     record_uuid = db.Column(UUIDType, nullable=False)
     """Snapshot record UUID."""
 
-    json = db.Column(db.JSON()
+    json = db.Column(
+        db.JSON()
         .with_variant(
             postgresql.JSONB(none_as_null=True),
             "postgresql",
@@ -39,12 +42,15 @@ class RecordSnapshot(db.Model):
         .with_variant(
             JSONType(),
             "mysql",
-    ),
+        ),
         default=lambda: dict(),
-        nullable=False, )
+        nullable=False,
+    )
     """JSON with current snapshot of the record."""
 
-    request_id = db.Column(UUIDType, db.ForeignKey(RequestMetadata.id, ondelete='cascade'), nullable=True)
+    request_id = db.Column(
+        UUIDType, db.ForeignKey(RequestMetadata.id, ondelete="cascade"), nullable=True
+    )
     """Request ID when snapshot is made."""
 
     request = db.relationship(RequestMetadata, foreign_keys=[request_id])
@@ -57,26 +63,22 @@ class RecordSnapshot(db.Model):
         """Create new snapshot."""
         try:
             with db.session.begin_nested():
-                obj = cls(
-                    record_uuid=record_uuid,
-                    request_id=request_id,
-                    json=json
-                )
+                obj = cls(record_uuid=record_uuid, request_id=request_id, json=json)
                 db.session.add(obj)
             logger.info(
-                'Created record snapshot for record uuid {record_uuid} with json {json}'
+                "Created record snapshot for record uuid {record_uuid} with json {json}"
             )
 
         except IntegrityError:
             logger.exception(
-                'Already exists',
-                extra={'record_uuid': record_uuid, 'json': json},
+                "Already exists",
+                extra={"record_uuid": record_uuid, "json": json},
             )
             raise
         except SQLAlchemyError:
             logger.exception(
-                'Failed to create record snapshot for record uuid {record_uuid}',
-                extra={'json': json}
+                "Failed to create record snapshot for record uuid {record_uuid}",
+                extra={"json": json},
             )
             raise
 
@@ -86,8 +88,7 @@ class RecordSnapshot(db.Model):
     def get(cls, record_uuid):
         """Get one snapshot for given record_uuid."""
         try:
-            args = dict(record_uuid=record_uuid)
-            return db.session.query(cls).filter_by(**args).one()
+            return db.session.query(cls).filter_by(record_uuid=record_uuid).one()
         except NoResultFound:
             raise ValueError(f"No record snapshot for record uuid {record_uuid}")
 
@@ -95,8 +96,7 @@ class RecordSnapshot(db.Model):
     def get_all(cls, record_uuid):
         """Get all snapshot for given record_uuid."""
         try:
-            args = dict(record_uuid=record_uuid)
-            return db.session.query(cls).filter_by(**args).all()
+            return db.session.query(cls).filter_by(record_uuid=record_uuid).all()
         except NoResultFound:
             raise ValueError(f"No record snapshots for record uuid {record_uuid}")
 
@@ -104,11 +104,12 @@ class RecordSnapshot(db.Model):
     def get_two_latest_snapshots_by_record_uuid(cls, record_uuid):
         """Get latest two snapshot for record_uuid."""
         try:
-            args = dict(record_uuid=record_uuid)
-            return (db.session.query(cls)
-                    .filter_by(**args)
-                    .order_by(cls.created.desc())
-                    .limit(2)
-                    .all())
+            return (
+                db.session.query(cls)
+                .filter_by(record_uuid=record_uuid)
+                .order_by(cls.created.desc())
+                .limit(2)
+                .all()
+            )
         except NoResultFound:
             raise ValueError(f"No record snapshot for record uuid {record_uuid}")
