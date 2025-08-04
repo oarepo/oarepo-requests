9948b303d7c2c9f99f533f5303fcf010db6cd2bb
Martin Čorovčák <martino.coro@gmail.com>
2025-08-04 15:01:13 +0200

    Refactor RequestButtons to group requests by nested category value and update accordion styling for improved layout

--- oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/record-requests/components/RequestButtons.jsx
+++ oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/record-requests/components/RequestButtons.jsx
@@ -10,27 +10,29 @@ import _partition from "lodash/partition";
 export const RequestButtons = ({ requests, mapRequestToModalComponent }) => {
   const [requestsWithCategory, requestsWithoutCategory] = _partition(requests, (request) => !_isNil(request?.category));
 
-  const groupedRequests = _groupBy(requestsWithCategory, "category");
+  const groupedRequests = _groupBy(requestsWithCategory, "category.value");
 
   const requestsWithoutCategoryGroup = _map(requestsWithoutCategory, mapRequestToModalComponent);
 
-  const requestsPerCategoryAccordions = _map(groupedRequests, (requests, category) => {
+  const requestsPerCategoryAccordions = _map(groupedRequests, (requests) => {
     const requestModalTriggers = _map(requests, mapRequestToModalComponent);
+    const category = requests[0].category; // All requests in the group have the same category
+    
     return (
       <Accordion
-        key={category}
+        key={category.value}
         className="requests-per-category"
         defaultActiveIndex={0}
         styled
         panels={[
           {
-            key: category,
+            key: category.value,
             title: {
-              content: category,
+              content: category.label,
             },
             content: {
               content: (
-                <Button.Group vertical labeled icon fluid attached="bottom">
+                <Button.Group vertical labeled icon fluid>
                   {requestModalTriggers}
                 </Button.Group>
               )
