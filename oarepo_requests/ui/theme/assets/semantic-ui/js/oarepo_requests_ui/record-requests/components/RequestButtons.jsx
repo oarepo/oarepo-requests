a12de271961ddad9147d7a12fb32b12cd1bd4432
Martin Čorovčák <martino.coro@gmail.com>
2025-08-11 16:22:10 +0200

    Refactor RequestButtons to handle single and multiple requests per category, improving UI layout and interaction. Add accordion variables for consistent styling.

--- oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/record-requests/components/RequestButtons.jsx
+++ oarepo_requests/ui/theme/assets/semantic-ui/js/oarepo_requests_ui/record-requests/components/RequestButtons.jsx
@@ -6,47 +6,58 @@ import _groupBy from "lodash/groupBy";
 import _map from "lodash/map";
 import _isNil from "lodash/isNil";
 import _partition from "lodash/partition";
+import _forEach from "lodash/forEach";
 
 export const RequestButtons = ({ requests, mapRequestToModalComponent }) => {
   const [requestsWithCategory, requestsWithoutCategory] = _partition(requests, (request) => !_isNil(request?.category));
 
-  const groupedRequests = _groupBy(requestsWithCategory, "category.value");
-
   const requestsWithoutCategoryGroup = _map(requestsWithoutCategory, mapRequestToModalComponent);
 
-  const requestsPerCategoryAccordions = _map(groupedRequests, (requests) => {
+  const groupedRequests = _groupBy(requestsWithCategory, "category.value");
+  
+  let requestsInCategoryAlone = [];
+  let requestsInCategoryMultiple = [];
+
+  _forEach(groupedRequests, (requests) => {
     const requestModalTriggers = _map(requests, mapRequestToModalComponent);
     const category = requests[0].category; // All requests in the group have the same category
     
-    return (
-      <Accordion
-        key={category.value}
-        className="requests-per-category"
-        defaultActiveIndex={0}
-        styled
-        panels={[
-          {
-            key: category.value,
-            title: {
-              content: category.label,
+    if (requests.length === 1) {
+      // If there's only one request in the category, return it directly
+      requestsInCategoryAlone.push(requestModalTriggers[0]);
+    } else {
+      // If there are multiple requests in the category, return them as a Accordion with a ButtonGroup
+      requestsInCategoryMultiple.push(
+        <Accordion
+          key={category.value}
+          className="requests-per-category"
+          defaultActiveIndex={0}
+          styled
+          panels={[
+            {
+              key: category.value,
+              title: {
+                content: category.label,
+              },
+              content: {
+                content: (
+                  <Button.Group vertical labeled icon fluid>
+                    {requestModalTriggers}
+                  </Button.Group>
+                )
+              },
             },
-            content: {
-              content: (
-                <Button.Group vertical labeled icon fluid>
-                  {requestModalTriggers}
-                </Button.Group>
-              )
-            },
-          },
-        ]}
-      />
-    );
+          ]}
+        />
+      );
+    }
   });
 
   return (
     <>
       {requestsWithoutCategoryGroup}
-      {requestsPerCategoryAccordions}
+      {requestsInCategoryAlone}
+      {requestsInCategoryMultiple}
     </>
   );
 };
