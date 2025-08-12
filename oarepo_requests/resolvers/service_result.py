3eb57ddde0b97394cc62c3584eb1ba6327f2bf7f
ron <kristron6@gmail.com>
2025-08-12 10:08:09 +0200

    rdm pid resolving in ServiceResultResolver

--- oarepo_requests/resolvers/service_result.py
+++ oarepo_requests/resolvers/service_result.py
@@ -2,14 +2,16 @@ from invenio_records_resources.references.entity_resolvers.results import (
     ServiceResultResolver,
 )
 
-
-class DraftServiceResultResolver(ServiceResultResolver):
+class RDMPIDServiceResultResolver(ServiceResultResolver):
     """Service result resolver for draft records."""
 
     def _reference_entity(self, entity):
         """Create a reference dict for the given result item."""
         pid = entity.id if isinstance(entity, self.item_cls) else entity.pid.pid_value
         return {self.type_key: str(pid)}
+
+class DraftServiceResultResolver(RDMPIDServiceResultResolver):
+    """Service result resolver for draft records."""
 
     @property
     def draft_cls(self):
