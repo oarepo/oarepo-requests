af9ffda67ec43e5bb6f3674c7def7ea8452d6d63
Martin Čorovčák <martino.coro@gmail.com>
2025-08-20 14:17:36 +0200

    Remove deprecated workflows and configuration files; update .gitignore and project metadata for version 3.0.0 based on https://github.com/oarepo/oarepo/blob/main/README.md

--- oarepo_requests/__init__.py
+++ oarepo_requests/__init__.py
@@ -1,8 +1,24 @@
 #
-# Copyright (C) 2024 CESNET z.s.p.o.
+# Copyright (c) 2025 CESNET z.s.p.o.
 #
-# oarepo-requests is free software; you can redistribute it and/or
-# modify it under the terms of the MIT License; see LICENSE file for more
-# details.
+# This file is a part of oarepo-ui (see https://github.com/oarepo/oarepo-ui).
 #
-"""Invenio extension for better handling of requests."""
+# oarepo-ui is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""OARepo Requests module initialization.
+
+This module provides the main entry point for the OARepo Requests extension
+of Invenio which adds better handling of requests.
+"""
+
+from __future__ import annotations
+
+from .ext import OARepoRequests
+
+__version__ = "3.0.0"
+
+__all__ = [
+    "OARepoRequests",
+    "__version__",
+]
