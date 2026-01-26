c64fa3dc9c0b29c4969a712e0d1ec6c318554209
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-01-26 14:52:43 +0100

    fixing tests

--- oarepo_requests/ui/theme/__init__.py
+++ oarepo_requests/ui/theme/__init__.py
@@ -1,10 +1,11 @@
 #
-# Copyright (C) 2024 CESNET z.s.p.o.
+# Copyright (c) 2026 CESNET z.s.p.o.
 #
-# oarepo-requests is free software; you can redistribute it and/or
-# modify it under the terms of the MIT License; see LICENSE file for more
-# details.
+# This file is a part of oarepo-requests (see https://github.com/oarepo/oarepo-requests).
 #
-"""UI semantic-ui theme for the module."""
+# oarepo-requests is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""Oarepo requests theme package."""
 
 from __future__ import annotations
