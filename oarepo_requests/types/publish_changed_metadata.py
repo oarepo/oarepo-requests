aad550bcdcd34d3ccf6954e163f494969411fb08
ron <kristron6@gmail.com>
2026-07-20 11:35:39 +0200

    handle ValueError in is_applicable_to methods across publish draft request types

--- oarepo_requests/types/publish_changed_metadata.py
+++ oarepo_requests/types/publish_changed_metadata.py
@@ -51,7 +51,10 @@ class PublishChangedMetadataRequestType(PublishRequestType):
     @classmethod
     def is_applicable_to(cls, identity: Identity, topic: Record, *args: Any, **kwargs: Any) -> bool:
         """Check if the request type is applicable to the topic."""
-        topic = cls.convert_topic(identity, topic)
+        try:
+            topic = cls.convert_topic(identity, topic)
+        except ValueError:
+            return False
         if cls.topic_type(topic) != "metadata":
             return False
 
